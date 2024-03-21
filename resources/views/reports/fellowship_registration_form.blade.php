c8bc3ba51f6a4de42a0e3e9d3f68a5d287f9dc57
Abhishek Singh <148897811+Abhi7251singh@users.noreply.github.com>
2024-03-21 19:30:54 +0530

    all done

--- resources/views/reports/fellowship_registration_form.blade.php
+++ resources/views/reports/fellowship_registration_form.blade.php
@@ -48,14 +48,14 @@
         'images' => $data['Upload_Photo'],
         'form_data' => [
             [
-                'top' => '334px',
+                'top' => '337px',
                 'left' => '232px',
                 'data' => $data['AccompanyingPersonsProfession1'] ?? '',
                 'type' => 'text',
             ],
-            ['top' => '335px', 'left' => '257px', 'data' => $data['Profession_name1'] ?? '', 'type' => 'text'],
-            ['top' => '368px', 'left' => '147px', 'data' => $data['address1'] ?? '', 'type' => 'text'],
-            ['top' => '437px', 'left' => '297px', 'data' => $data['institution_name'] ?? '', 'type' => 'text'],
+            ['top' => '337px', 'left' => '257px', 'data' => $data['Profession_name1'] ?? '', 'type' => 'text'],
+            ['top' => '370px', 'left' => '147px', 'data' => $data['address1'] ?? '', 'type' => 'text'],
+            ['top' => '438px', 'left' => '297px', 'data' => $data['institution_name'] ?? '', 'type' => 'text'],
             ['top' => '472px', 'left' => '162px', 'data' => $data['City'] ?? '', 'type' => 'text'],
             ['top' => '472px', 'left' => '544px', 'data' => $data['State'] ?? '', 'type' => 'text'],
             ['top' => '506px', 'left' => '143px', 'data' => $data['MobileNumber'] ?? '', 'type' => 'text'],
