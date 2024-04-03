2c948c9619147c32d6b34b0a7cccff2d894079a3
Abhishek Singh <148897811+Abhi7251singh@users.noreply.github.com>
2024-04-03 18:49:45 +0530

    fixed

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -98,16 +98,16 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/4.5.0/fabric.min.js"></script>
     <script src="{{ asset('/js/scripts.js') }}"></script>
     <script>
-        if ($('[name="txn_id"]').length != 0) {
-            $('button').attr('disabled', true)
-        }
-        /*$('[name="txn_id"]').on('keyup', function() {
-            if ($(this).val().length == 0) {
+        /*if ($('[name="txn_id"]').length != 0) {
                 $('button').attr('disabled', true)
-            } else {
-                $('button').attr('disabled', false)
             }
-        })*/
+            $('[name="txn_id"]').on('keyup', function() {
+                if ($(this).val().length == 0) {
+                    $('button').attr('disabled', true)
+                } else {
+                    $('button').attr('disabled', false)
+                }
+            })*/
         document.getElementsByClassName('preview_image')[0].style.display = 'none';
         document.addEventListener('DOMContentLoaded', () => {
             const canvas = new fabric.Canvas('canvas');
